3a1a1bff1f9b664774cebff41f355797e62a4b69
Podshot <ben.gothard3@gmail.com>
2014-09-02 21:00:25 -0400

    Development mode no longer checks for updates

--- src/io/github/podshot/Podlauncher/UpdateChecker.java
+++ src/io/github/podshot/Podlauncher/UpdateChecker.java
@@ -11,31 +11,33 @@ import org.json.simple.parser.JSONParser;
 import sk.tomsik68.mclauncher.util.HttpUtils;
 
 public class UpdateChecker {
-	
+
 	private String versionInfoURL = "http://podshot.github.io/PodLauncher/update/update.json";
 	private String currentVersion = PodLauncher.getVersion();
 	private static boolean canidateUpdate;
 	private static URL updaterURL;
-	
+
 	public UpdateChecker() throws Exception {
-		
+
 		String versionInfoSTR = HttpUtils.httpGet(versionInfoURL);
-		
+
 		JSONObject versionInfo = (JSONObject) new JSONParser().parse(versionInfoSTR);
-		
+
 		String updateVersion = (String) versionInfo.get("Version");
 		String updateStage = (String) versionInfo.get("Development Stage");
 		updaterURL = new URL((String) versionInfo.get("Updater URL"));
-		
-		if (!(currentVersion.equals(updateVersion))) {
-			if (updateStage.equalsIgnoreCase("stable")) {
-				UpdateGUI upGUI = new UpdateGUI(updateVersion, false);
-				upGUI.setVisible(true);
-			}
-			if (updateStage.equalsIgnoreCase("CANIDATE") && LauncherConfig.shouldUseCanidateBuilds()) {
-				canidateUpdate = true;
-				UpdateGUI upGUI = new UpdateGUI(updateVersion, true);
-				upGUI.setVisible(true);
+
+		if (!(PodLauncher.isDevMode())) {
+			if (!(currentVersion.equals(updateVersion))) {
+				if (updateStage.equalsIgnoreCase("stable")) {
+					UpdateGUI upGUI = new UpdateGUI(updateVersion, false);
+					upGUI.setVisible(true);
+				}
+				if (updateStage.equalsIgnoreCase("CANIDATE") && LauncherConfig.shouldUseCanidateBuilds()) {
+					canidateUpdate = true;
+					UpdateGUI upGUI = new UpdateGUI(updateVersion, true);
+					upGUI.setVisible(true);
+				}
 			}
 		}
 	}
@@ -43,7 +45,7 @@ public class UpdateChecker {
 	public static URL getUpdateURL() {
 		return updaterURL;
 	}
-	
+
 	public static boolean isACanidateUpdate() {
 		return canidateUpdate;
 	}
