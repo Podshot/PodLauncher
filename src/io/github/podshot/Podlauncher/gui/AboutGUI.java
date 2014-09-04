dd6247650dd3d36df9a126beca8359de4b2688c0
Podshot <ben.gothard3@gmail.com>
2014-09-04 19:58:26 -0400

    More AboutGUI stuff

--- src/io/github/podshot/Podlauncher/gui/AboutGUI.java
+++ src/io/github/podshot/Podlauncher/gui/AboutGUI.java
@@ -17,17 +17,20 @@ public class AboutGUI extends JFrame implements ActionListener {
 		this.setTitle("About PodLauncher");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.getContentPane().setLayout(null);
+		this.setSize(450, 380);
 		
 		btnCloseThisWindow = new JButton("Close this Window");
 		btnCloseThisWindow.setBounds(10, 11, 130, 23);
+		btnCloseThisWindow.addActionListener(this);
 		getContentPane().add(btnCloseThisWindow);
 		
 		JTextPane txtpnPodlauncherIsA = new JTextPane();
 		txtpnPodlauncherIsA.setContentType("text/html");
-		txtpnPodlauncherIsA.setText("<div align=\"center\">\r\n<h4>About PodLauncher</h4>\r\nPodLauncher is a Open Source launcher for Minecraft. The source code for this launcher is available on its <a href=\"http://github.com/Podshot/PodLauncher\">Github Repository</a>. Anyone is welcome to add ideas, code, or support. PodLauncher's goal is to create an easy to use, yet very customizable launcher,\r\n</div>");
+		txtpnPodlauncherIsA.setText("<div align=\"center\">\r\n<h2>About PodLauncher</h2>\r\nPodLauncher is a Open Source launcher for Minecraft. The source code for this launcher is available on its <a href=\"http://github.com/Podshot/PodLauncher\">Github Repository</a>. Anyone is welcome to add ideas, code, or support. PodLauncher's goal is to create an easy to use, yet very customizable launcher.\r\n\r\n<h2>Resources</h2>\r\n-<a href=\"http://github.com/tomsik68/mclauncher-api\">mclauncher-api</a> by Tomsik68\r\n<br>\r\n-<a href=\"http://github.com/Podshot/PodLauncher\">PodLauncher Source Code</a>\r\n<br>\r\n-<a href=\"https://github.com/Podshot/PodLauncher-Updater\">PodLauncher Updater Source Code</a>\r\n</div>");
 		txtpnPodlauncherIsA.setBackground(new Color(240, 240, 240));
 		txtpnPodlauncherIsA.setEditable(false);
-		txtpnPodlauncherIsA.setBounds(10, 45, 414, 205);
+		txtpnPodlauncherIsA.setAutoscrolls(true);
+		txtpnPodlauncherIsA.setBounds(10, 45, 414, 285);
 		getContentPane().add(txtpnPodlauncherIsA);
 		
 		
