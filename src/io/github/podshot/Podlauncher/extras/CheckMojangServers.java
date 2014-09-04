f23ac47e959981c3815a522ff6c8a70ace2d3015
Podshot <ben.gothard3@gmail.com>
2014-09-03 21:47:26 -0400

    CheckMojangServers documentation

--- src/io/github/podshot/Podlauncher/extras/CheckMojangServers.java
+++ src/io/github/podshot/Podlauncher/extras/CheckMojangServers.java
@@ -12,6 +12,11 @@ public class CheckMojangServers {
 	
 	private static JSONParser parser = new JSONParser();
 	
+	/**
+	 * Converts a status string into a ServerStatuc ENUM, for easier status handling in MainGUI
+	 * @param stat String to convert to ENUM, suitable strings are 'green', 'yellow' or 'red'
+	 * @return A ServerStatus ENUM value
+	 */
 	@Utility(UtilityType.OTHER)
 	private static ServerStatus convertStatusToENUM(String stat) {
 		if (stat.equals("green")) {
@@ -24,6 +29,10 @@ public class CheckMojangServers {
 		return ServerStatus.OFFLINE;
 	}
 	
+	/**
+	 * Gets the Mojang JSON Server Status JSON from status.mojang.com/check
+	 * @return A JSONArray of the server statuses
+	 */
 	private static JSONArray getStatusJSON() {
 		JSONArray status = null;
 		try {
@@ -36,36 +45,59 @@ public class CheckMojangServers {
 		
 	}
 
+	/**
+	 * Gets Minecraft.net's status
+	 * @return A ServerStatus ENUM value
+	 */
 	public static ServerStatus getMinecraft_net() {
 		JSONArray status = getStatusJSON();
 		String statusCode = (String) ((JSONObject) status.get(0)).get("minecraft.net");
 		return convertStatusToENUM(statusCode);
 	}
 	
+	/**
+	 * Gets Mojang's Session servers status
+	 * @return A ServerStatus ENUM value
+	 */
 	public static ServerStatus getSessions() {
 		JSONArray status = getStatusJSON();
 		String statusCode = (String) ((JSONObject) status.get(1)).get("session.minecraft.net");
 		return CheckMojangServers.convertStatusToENUM(statusCode);
 	}
 	
+	/**
+	 * Gets Mojang's Account servers status
+	 * @return A ServerStatus ENUM value
+	 */
 	public static ServerStatus getAccounts() {
 		JSONArray status = getStatusJSON();
 		String statusCode = (String) ((JSONObject) status.get(2)).get("account.mojang.com");
 		return CheckMojangServers.convertStatusToENUM(statusCode);
 	}
 	
+	/**
+	 * Gets Mojang's Authentication servers status
+	 * @return A ServerStatus ENUM value
+	 */
 	public static ServerStatus getAuth() {
 		JSONArray status = getStatusJSON();
 		String statusCode = (String) ((JSONObject) status.get(3)).get("auth.mojang.com");
 		return CheckMojangServers.convertStatusToENUM(statusCode);
 	}
 	
+	/**
+	 * Gets Mojang's Skin servers status
+	 * @return A ServerStatus ENUM value
+	 */
 	public static ServerStatus getSkins() {
 		JSONArray status = getStatusJSON();
 		String statusCode = (String) ((JSONObject) status.get(4)).get("skins.minecraft.net");
 		return CheckMojangServers.convertStatusToENUM(statusCode);
 	}
 	
+	/**
+	 * Server Status ENUM, for easier handling in MainGUI
+	 */
 	public enum ServerStatus {
 		ONLINE, UNSTABLE, OFFLINE;
 	}
