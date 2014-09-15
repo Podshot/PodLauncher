57fd1735e6a0bf5852e840571dc0239e826fa90e
Podshot <ben.gothard3@gmail.com>
2014-09-15 18:12:12 -0400

    ErrorGUI now makes an errors subfolder

--- src/io/github/podshot/Podlauncher/gui/ErrorGUI.java
+++ src/io/github/podshot/Podlauncher/gui/ErrorGUI.java
@@ -17,11 +17,20 @@ public class ErrorGUI extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * A GUI used to notify the user that an Exception occurred
+	 * @param e The Exception that was thrown
+	 */
 	public ErrorGUI(Exception e) {
+		// Gets the current date and time, so I can format the log file name
 		Calendar cal = Calendar.getInstance();
 		java.util.Date now = cal.getTime();
 		Timestamp current = new Timestamp(now.getTime());
-		File log = new File("PodLauncher" + File.separator + "error_" + current.toString().replace(" ", "_").replace(":", "-") + ".log");
+		File errorFolder = new File("PodLauncher" + File.separator + "errors");
+		if (!(errorFolder.exists())) {
+			errorFolder.mkdir();
+		}
+		File log = new File("PodLauncher" + File.separator + "errors" + File.separator + "error_" + current.toString().replace(" ", "_").replace(":", "-") + ".log");
 		try {
 			log.createNewFile();
 		} catch (IOException e2) {
@@ -45,12 +54,12 @@ public class ErrorGUI extends JFrame implements ActionListener {
 		lblAnErrorHas.setBounds(10, 11, 375, 14);
 		getContentPane().add(lblAnErrorHas);
 		
-		JLabel lblPleaseSeeThe = new JLabel("Please see " + log.getName() + " in the PodLauncher folder for");
+		JLabel lblPleaseSeeThe = new JLabel("Please see " + log.getName() + " in the PodLauncher error");
 		lblPleaseSeeThe.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPleaseSeeThe.setBounds(10, 36, 375, 14);
 		getContentPane().add(lblPleaseSeeThe);
 		
-		JLabel lblMoreDetails = new JLabel("more details");
+		JLabel lblMoreDetails = new JLabel("folder for more details");
 		lblMoreDetails.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMoreDetails.setBounds(10, 61, 375, 14);
 		getContentPane().add(lblMoreDetails);
