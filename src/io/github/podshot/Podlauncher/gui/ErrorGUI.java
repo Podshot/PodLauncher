6ae937b9bdac5d85190f421b2978ca070c2776aa
Podshot <ben.gothard3@gmail.com>
2014-09-15 08:23:55 -0400

    ErrorGUI now write the entire Exception to a log file

--- src/io/github/podshot/Podlauncher/gui/ErrorGUI.java
+++ src/io/github/podshot/Podlauncher/gui/ErrorGUI.java
@@ -1,61 +1,61 @@
 package io.github.podshot.Podlauncher.gui;
 
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 
 public class ErrorGUI extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-	public ErrorGUI(String msg, StackTraceElement[] stackTrace, Throwable throwableCause, String caughtBy) {
+	public ErrorGUI(Exception e) {
+		Calendar cal = Calendar.getInstance();
+		java.util.Date now = cal.getTime();
+		Timestamp current = new Timestamp(now.getTime());
+		File log = new File("PodLauncher" + File.separator + "error_" + current.toString().replace(" ", "_").replace(":", "-") + ".log");
+		try {
+			log.createNewFile();
+		} catch (IOException e2) {
+			System.out.println("Could not create file");
+		}
+		try {
+			PrintWriter pw = new PrintWriter(log);
+			e.printStackTrace(pw);
+			pw.flush();
+			pw.close();
+		} catch (FileNotFoundException e1) {
+			System.out.println("Could not find file");
+		}
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setType(Type.POPUP);
 		this.setTitle("An Error Occured");
 		getContentPane().setLayout(null);
-		this.setSize(500, 280);
+		
+		JLabel lblAnErrorHas = new JLabel("An Error has occured while PodLauncher was running.");
+		lblAnErrorHas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAnErrorHas.setBounds(10, 11, 375, 14);
+		getContentPane().add(lblAnErrorHas);
+		
+		JLabel lblPleaseSeeThe = new JLabel("Please see " + log.getName() + " in the PodLauncher folder for");
+		lblPleaseSeeThe.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseSeeThe.setBounds(10, 36, 375, 14);
+		getContentPane().add(lblPleaseSeeThe);
+		
+		JLabel lblMoreDetails = new JLabel("more details");
+		lblMoreDetails.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMoreDetails.setBounds(10, 61, 375, 14);
+		getContentPane().add(lblMoreDetails);
+		this.setSize(411, 141);
 		this.setAlwaysOnTop(true);
-
-		JLabel lblAnErrorOccured = new JLabel("An Error occured while PodLauncher was running:");
-		lblAnErrorOccured.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblAnErrorOccured.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAnErrorOccured.setBounds(51, 11, 349, 33);
-		getContentPane().add(lblAnErrorOccured);
-
-		JLabel lblErrorName = new JLabel("Error Name: ");
-		lblErrorName.setHorizontalAlignment(SwingConstants.CENTER);
-		lblErrorName.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblErrorName.setBounds(10, 55, 83, 14);
-		getContentPane().add(lblErrorName);
-
-		JLabel labelErrorName = new JLabel(msg);
-		labelErrorName.setBounds(90, 55, 384, 14);
-		labelErrorName.setToolTipText(msg);
-		getContentPane().add(labelErrorName);
-
-		JLabel lblCaughtBy = new JLabel("Caught By: ");
-		lblCaughtBy.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblCaughtBy.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCaughtBy.setBounds(10, 80, 83, 14);
-		getContentPane().add(lblCaughtBy);
-
-		JLabel labelCaughtBy = new JLabel(caughtBy);
-		labelCaughtBy.setBounds(90, 80, 290, 14);
-		getContentPane().add(labelCaughtBy);
-
-		JTextPane textArea = new JTextPane();
-		textArea.setEditable(false);
-		textArea.setAutoscrolls(true);
-		for (StackTraceElement trace : stackTrace) {
-			textArea.setText(textArea.getText() + trace.getClassName() + ":" + trace.getFileName() + ":" + trace.getMethodName() + ":" + trace.getLineNumber() + "\n");
-		}
-		textArea.setBounds(10, 105, 464, 125);
-		getContentPane().add(textArea);
 		this.setVisible(true);
 	}
 
