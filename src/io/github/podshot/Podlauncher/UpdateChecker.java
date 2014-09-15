77d26c7ce3ce16c0ed1f27a389f6c4d3900bcb37
Podshot <ben.gothard3@gmail.com>
2014-09-15 10:09:59 -0400

    New Canidate Build checking

--- src/io/github/podshot/Podlauncher/UpdateChecker.java
+++ src/io/github/podshot/Podlauncher/UpdateChecker.java
@@ -1,5 +1,7 @@
 package io.github.podshot.Podlauncher;
 
+import io.github.podshot.Podlauncher.extras.Utility;
+import io.github.podshot.Podlauncher.extras.Utility.UtilityType;
 import io.github.podshot.Podlauncher.files.LauncherConfig;
 import io.github.podshot.Podlauncher.gui.UpdateGUI;
 
@@ -17,35 +19,44 @@ public class UpdateChecker {
 	private static boolean canidateUpdate;
 	private static URL updaterURL;
 
+	@Utility(UtilityType.DEVELOPMENT)
 	public UpdateChecker() throws Exception {
+		boolean usingCanidateUpdate = false;
 
 		String versionInfoSTR = HttpUtils.httpGet(versionInfoURL);
 
 		JSONObject versionInfo = (JSONObject) new JSONParser().parse(versionInfoSTR);
 
 		String updateVersion = (String) versionInfo.get("Version");
-		String updateStage = (String) versionInfo.get("Development Stage");
 		updaterURL = new URL((String) versionInfo.get("Updater URL"));
+		if (LauncherConfig.shouldUseCanidateBuilds()) {
+			JSONObject canidateInfo = (JSONObject) versionInfo.get("Canidate");
+			updateVersion = (String) canidateInfo.get("Version");
+			updaterURL = new URL((String) canidateInfo.get("Updater URL"));
+			usingCanidateUpdate = true;
+			canidateUpdate = true;
+		}
 
 		if (!(PodLauncher.isDevMode())) {
 			if (!(currentVersion.equals(updateVersion))) {
-				if (updateStage.equalsIgnoreCase("stable")) {
-					UpdateGUI upGUI = new UpdateGUI(updateVersion, false);
-					upGUI.setVisible(true);
-				}
-				if (updateStage.equalsIgnoreCase("CANIDATE") && LauncherConfig.shouldUseCanidateBuilds()) {
-					canidateUpdate = true;
-					UpdateGUI upGUI = new UpdateGUI(updateVersion, true);
-					upGUI.setVisible(true);
-				}
+				UpdateGUI upGUI = new UpdateGUI(updateVersion, usingCanidateUpdate);
+				upGUI.setVisible(true);
 			}
 		}
 	}
 
+	/**
+	 * Gets the URL of the PodLauncher Updater
+	 * @return The URL of the Updater
+	 */
 	public static URL getUpdateURL() {
 		return updaterURL;
 	}
 
+	/**
+	 * Checks if the found update is a Canidate Build
+	 * @return True if the update is a canidate build
+	 */
 	public static boolean isACanidateUpdate() {
 		return canidateUpdate;
 	}
